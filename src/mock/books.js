70eeed6e2cb97ba6c42a78a3a68d21c98417ea69
Valero-123 <era_ich@mail.ru>
2025-11-07 12:25:56 +0300

    Update books.js

--- src/mock/books.js
+++ src/mock/books.js
@@ -4,12 +4,13 @@ export const books = [
         id: 1,
         author: "author2",
         title: "name2",
-        genre: "Science"
+        genre: "Выполнено"
     },
         {
         id: 2,
         author: "author1",
         title: "name4",
-        genre: "Fiction"
+        genre: "Не выполнено"
     }
 ]
+
